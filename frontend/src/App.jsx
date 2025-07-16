b20aca0483ea4f3477525a9dc1c6662ce39057e5
indrajeet bhujabl <indrajeetbhujbal0@gmail.com>
2025-07-16 21:35:37 +0530

    sloved bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import {Routes,Route} from 'react-router-dom'
 import About from './components/About'
 import Contact from './components/Contact'
 import Home from './components/Home.jsx'
-import Products from './components/products.jsx'
+import Product from './components/Product.jsx'
 const App = () => {
   return (
     <>
@@ -15,7 +15,7 @@ const App = () => {
         <Route path='/' element={<Home></Home>}/>
         <Route path='/about' element={<About></About>}/>
 
-        <Route path='/products' element={<Products></Products>}/>
+        <Route path='/products' element={<Product></Product>}/>
         <Route path='/contact' element={<Contact></Contact>}/>
 
       </Routes>
